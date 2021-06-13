8df3e4e826605da2317530bbc7d3044a8c6286a2
ThiagoViek <ThiagoViek@github.com>
2021-06-13 11:58:58 +0200

    pre-release develop

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import argparse
 from src.resizer import Resizer
 
 def main() -> None:
-    resizer = Resizer(3)
+    # Inputs
+    parser = argparse.ArgumentParser(description="-- Image Resizer --")
+    parser.add_argument("--resize_ratio",
+                        help = "Ratio by which the image is resize",
+                        type = int,
+                        default = 3)
+    args = parser.parse_args()
+    ratio = args.resize_ratio
+
+    # Resizing Images
+    resizer = Resizer(ratio)
     resizer.resize_images()
 
 if __name__ == "__main__":
