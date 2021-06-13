60cdcd5da6a50d0e191a860f972ae8a4ec9a55f6
ThiagoViek <ThiagoViek@github.com>
2021-06-13 10:54:42 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.resizer import Resizer
+
+def main() -> None:
+    src_dir = 0
+
+if __name__ == "__main__":
+    main()
---
+++ src/resizer.py
@@ -0,0 +1,4 @@
+class Resizer:
+    def __init__(self) -> None:
+        pass
+    
